ec9458ca2883e128fde00d1b4be22ffce3f13495
T29VN <baitap29012004@gmail.com>
2025-10-28 14:09:49 +0700

    hbjbhjbjb

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'Test')
+BEGIN
+    CREATE DATABASE Test;
+END
+GO
+ 
+USE Test;
+GO
+ 
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE name = 'Vector2    ')
+BEGIN
+    CREATE TABLE Vector2 (
+        a INT,
+        b INT
+    );
+END
+GO
